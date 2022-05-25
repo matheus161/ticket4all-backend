80a9fcbaa59afca95949db4fe54371e1aead961e
Matheus <matheeus161@gmail.com>
2022-05-25 10:35:57 -0300

    add: CRUD Categoria, CRUD Evento e CR Organizador

---
+++ src/models/Evento.js
@@ -0,0 +1,42 @@
+import Joi from 'joi';
+import mongoose, { Schema, model } from 'mongoose';
+
+const EventoSchema = new Schema(
+    {
+        titulo: {
+            type: String,
+            required: true
+        },
+        descricao: {
+            type: String,
+        },
+        datas: [{
+            type: String
+        }],
+        organizador: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Organizador',
+            required: true
+        },
+        categoria: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Categoria'
+        }],
+    },
+
+    { timeStamps: true },
+);
+
+// Criando uma model
+const Evento = model('Evento', EventoSchema);
+
+// Fazendo a verificação dos atributos direto na rota
+const eventoRules = Joi.object({
+    titulo: Joi.string().required(),
+    descricao: Joi.string(),
+    datas: Joi.array(),
+    organizador: Joi.string().required(),
+    categoria: Joi.array()
+});
+
+export { Evento, eventoRules };
